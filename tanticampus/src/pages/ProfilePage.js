fc7a54549219f4306c669cc30b2551c67aa9d86a
Djams0 <mansourdjamil14@gmail.com>
2025-07-03 10:18:23 +0200

    commit 2

--- tanticampus/src/pages/ProfilePage.js
+++ tanticampus/src/pages/ProfilePage.js
@@ -1,11 +1,40 @@
-import React from 'react';
-import './ProfilePage.css';
+import React from "react";
+import "./ProfilePage.css";
+import { FaUser, FaWallet, FaCoins, FaCalendarAlt, FaCog, FaQuestionCircle, FaEdit } from "react-icons/fa";
+import { RiGroup2Line } from "react-icons/ri";
 
 const ProfilePage = () => {
+  const menuItems = [
+    { icon: <FaUser />, label: "Mon profil" },
+    { icon: <RiGroup2Line />, label: "Mes tontines" },
+    { icon: <FaWallet />, label: "Mon Wallet" },
+    { icon: <FaCoins />, label: "Code Privilège" },
+    { icon: <FaCalendarAlt />, label: "Mes évènements" },
+    { icon: <FaCog />, label: "Paramètres" },
+    { icon: <FaQuestionCircle />, label: "Aide" },
+  ];
+
   return (
-    <div className="profile-page">
-      <h1>Mon Profil</h1>
-      {/* Informations du profil */}
+    <div className="profile-container">
+      <header className="profile-header">
+        <div className="avatar" />
+        <h2 className="user-name">Mansour Djamil Ndiaye</h2>
+      </header>
+
+      <div className="profile-menu">
+        {menuItems.map((item, index) => (
+          <div key={index} className="menu-item">
+            <div className="item-left">
+              <span className="icon">{item.icon}</span>
+              <span className="label">{item.label}</span>
+            </div>
+            <div className="item-actions">
+              <span className="check">✔</span>
+              <FaEdit className="edit-icon" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
