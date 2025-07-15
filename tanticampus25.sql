cc35d0edd1598a7752b0f510298fc08c1ee5c35f
Djams0 <mansourdjamil14@gmail.com>
2025-07-15 18:35:19 +0200

    commit 2

--- tanticampus25.sql
+++ tanticampus25.sql
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `group_messages` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
-
+ 
 --
 -- Structure de la table `join_requests`
 --
